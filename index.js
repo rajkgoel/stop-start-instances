286d3cc5479aa31d8e1dc354d87c7348705b65ea
Raj Goel <rajkgoel@gmail.com>
2022-08-27 17:00:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { StopInstancesCommand } = require("@aws-sdk/client-ec2")
 exports.handler = async (event) => {
     console.log("Stopping EC2 instances.")
     var params = {
-        InstanceIds: [ 'i-0ffccf711611c718f', 'i-04eb54af89bebb45e' , 'i-003dbfb76fc965b8f', 'i-00fae5130e9ce4406', 'i-0d6812bc52a0c2bb5'],
+        InstanceIds: [ ''],
         Force: false,
     };
     try {
